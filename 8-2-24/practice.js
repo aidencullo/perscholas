1aec692ad741dfdf3b110824e9983ceff9cfbd2c
Aiden Cullo <aidencullo@gmail.com>
2024-08-02 14:46:24 -0400

    remove comments and format

--- 8-2-24/practice.js
+++ 8-2-24/practice.js
@@ -1,32 +1,13 @@
-// Practice Activity 1: Write a Function Declaration
-// Write a function named computeArea using the function declaration approach.
-
-// It will have two parameters: width & height.
-// It will compute the area of a rectangle (width multiplied by height) and return a string in the following form:
-
-// The area of a rectangle with a width of _ and a height of _ is ___ square units.
-
 function computeArea(width, height) {
-	return `The area of a rectangle with a width of ${width} and a height of ${height} is ${width * height} square units.`;
+  return `The area of a rectangle with a width of ${width} and a height of ${height} is ${width * height} square units.`;
 }
 
 const result = computeArea(5, 10); // The area of a rectangle with a width of 5 and a height of 10 is 50 square units.
 
 console.log(result);
 
-
-// Invoke the function to test it.
-
-// Practice Activity 2: Write a Function Expression
-// Write a function named planetHasWater using the function expression syntax.
-
-// It will have one parameter: planet.
-// Return true if the planet argument is either "Earth" or "Mars", otherwise return false.
-// Bonus points if you ensure the function will work regardless of the casing of the planet being passed in ('earth', 'MARS', etc.).
-// Invoke the function a couple of times to test it!
-
 const planetHasWater = function(planet) {
-	return planet.toLowerCase() === 'earth' || planet.toLowerCase() === 'mars';
+  return planet.toLowerCase() === 'earth' || planet.toLowerCase() === 'mars';
 }
 
 const planetA = 'Earth';
