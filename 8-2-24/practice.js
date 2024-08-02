b5a8ac33f488db7ab34b51b28e6699f8823254b0
Aiden Cullo <aidencullo@gmail.com>
2024-08-02 14:51:07 -0400

    standardize function call

--- 8-2-24/practice.js
+++ 8-2-24/practice.js
@@ -2,15 +2,18 @@ function computeArea(width, height) {
   return `The area of a rectangle with a width of ${width} and a height of ${height} is ${width * height} square units.`;
 }
 
-const result = computeArea(5, 10); // The area of a rectangle with a width of 5 and a height of 10 is 50 square units.
-
+const result = computeArea(5, 10);
 console.log(result);
 
-const planetHasWater = function(planet) {
+function planetHasWater(planet, justForShow) {
   return planet.toLowerCase() === 'earth' || planet.toLowerCase() === 'mars';
 }
 
 const planetA = 'Earth';
-console.log(`${planetA} has water: ${planetHasWater(planetA)}`); // Earth has water: true
+console.log(`${planetA} has water: ${planetHasWater(planetA)}`);
 const planetB = 'Mars';
-console.log(`${planetB} has water: ${planetHasWater(planetB)}`); // Mars has water: true
+console.log(`${planetB} has water: ${planetHasWater(planetB)}`);
+const planetC = 'Jupiter';
+console.log(`${planetC} has water: ${planetHasWater(planetC)}`);
+const planetD = 'MARS';
+console.log(`${planetD} has water: ${planetHasWater(planetD)}`);
