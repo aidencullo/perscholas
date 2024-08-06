b2cccf09246e58144079e0d3a1932193d186752c
Aiden Cullo <aidencullo@gmail.com>
2024-08-06 15:12:14 -0400

    convert to array of objects

--- ALAB308.4.1/main.js
+++ ALAB308.4.1/main.js
@@ -5,4 +5,21 @@ const results = [["ID", "Name", "Occupation", "Age"],  ["42", "Bruce", "Knight
 const test = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
 const twoDArray = test.split("\n").map((el) => el.split(","));
-console.log(twoDArray);
+
+// part 3
+
+const test_input = [["ID", "Name", "Occupation", "Age"],  ["42", "Bruce", "Knight", "41"],  ["57", "Bob", "Fry Cook", "19"],  ["63", "Blaine", "Quiz Master", "58"],  ["98", "Bill", "Doctor’s Assistant", "26"]]
+const test_output = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },  { id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26" }]
+
+const attrs = test_input[0]
+const result = []
+
+for (const row of test_input.splice(1)) {
+  const obj = {}
+  for (const attr of attrs) {
+    obj[attr.toLowerCase()] = row[attrs.indexOf(attr)]
+  }
+  result.push(obj)
+}
+
+console.log(result);
