0cf29cec8f4c3425272f79a628cd86a35d004ae2
Aiden Cullo <aidencullo@gmail.com>
2024-08-20 15:23:33 -0400

    create bank class to demonstrate instance methods

---
+++ class/8-20/instance.js
@@ -0,0 +1,29 @@
+class BankAccount {
+  constructor(balance) {
+    this.balance = balance;
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+  }
+
+  withdraw(amount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+    } else {
+      console.log('Insufficient funds.');
+    }
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+}
+
+// Create an instance of BankAccount
+const account = new BankAccount(1000);
+
+// Modify state using instance methods
+account.deposit(500);
+account.withdraw(200);
+console.log(account.getBalance());  // Output: 1300
