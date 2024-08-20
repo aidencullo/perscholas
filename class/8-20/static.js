9a82b3ef91b4545e6bec1adae861486020f8ae1d
Aiden Cullo <aidencullo@gmail.com>
2024-08-20 15:13:25 -0400

    cat inherits animal obj

--- class/8-20/static.js
+++ class/8-20/static.js
@@ -1,8 +1,66 @@
-class MyClass {
-  static classMethod() {
-    console.log('This is a class method.');
+class Animal {
+	constructor(eyes, legs, isAwake, isMoving) {
+		this.eyes = eyes;
+    this.legs = legs;
+    this.isAwake = isAwake;
+    this.isMoving = isMoving;
+	  // return is not needed because the new object is returned by default
+	}
+  sleep() {
+    this.isAwake = false;
+  }
+  wake() {
+    this.isAwake = true;
+  }
+  sit() {
+    this.isMoving = false;
+  }
+  walk() {
+    this.isMoving = true;
+  }
+  speak(sound) {
+    console.log(sound);
+  }
+  toString(animal = 'Animal') {
+    return `This ${animal} has ${this.eyes} eyes and ${this.legs} legs. It ${this.isAwake ? 'is' : 'is not'} awake, and ${this.isMoving ? 'is' : 'is not'} moving.`;
   }
 }
 
-// Call the class method directly on the class
-MyClass.classMethod(); // Output: This is a class method.
+class Cat extends Animal {
+  constructor(fur, isAwake, isMoving) {
+    super(2, 4, isAwake, isMoving);
+    this.fur = fur;
+  }
+  speak() {
+    super.speak("Meow...");
+  }
+  toString() {
+    return super.toString("Cat");
+  }
+}
+
+class Dog extends Animal {
+  constructor(fur, isAwake, isMoving) {
+    super(2, 4, isAwake, isMoving);
+    this.fur = fur;
+  }
+  speak() {
+    super.speak("Woof!");
+  }
+  toString() {
+    return super.toString("Dog");
+  }
+}
+
+class Cow extends Animal {
+  constructor(hair, isAwake, isMoving) {
+    super(2, 4, isAwake, isMoving);
+    this.hair = hair;
+  }
+  speak() {
+    super.speak("Moo.");
+  }
+  toString() {
+    return super.toString("Cow");
+  }
+}
