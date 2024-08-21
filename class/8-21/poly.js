a7dc2922e58045bfe86e50b0c912b50526c83a2e
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 14:11:29 -0400

    climb prototype chain

--- class/8-21/poly.js
+++ class/8-21/poly.js
@@ -1,75 +1,102 @@
-class Animal {
-	constructor(eyes, legs, isAwake, isMoving) {
-		this.eyes = eyes;
-    this.legs = legs;
-    this.isAwake = isAwake;
-    this.isMoving = isMoving;
-	}
-  sleep() {
-    this.isAwake = false;
-  }
-  wake() {
-    this.isAwake = true;
-  }
-  sit() {
-    this.isMoving = false;
-  }
-  walk() {
-    this.isMoving = true;
-  }
-  speak(sound) {
-    console.log(sound);
-  }
-  toString(animal = 'Animal') {
-    return `This ${animal} has ${this.eyes} eyes and ${this.legs} legs. It ${this.isAwake ? 'is' : 'is not'} awake, and ${this.isMoving ? 'is' : 'is not'} moving.`;
-  }
-}
+// class Animal {
+// 	constructor(eyes, legs, isAwake, isMoving) {
+// 		this.eyes = eyes;
+//     this.legs = legs;
+//     this.isAwake = isAwake;
+//     this.isMoving = isMoving;
+// 	}
+//   sleep() {
+//     this.isAwake = false;
+//   }
+//   wake() {
+//     this.isAwake = true;
+//   }
+//   sit() {
+//     this.isMoving = false;
+//   }
+//   walk() {
+//     this.isMoving = true;
+//   }
+//   speak(sound) {
+//     console.log(sound);
+//   }
+//   toString(animal = 'Animal') {
+//     return `This ${animal} has ${this.eyes} eyes and ${this.legs} legs. It ${this.isAwake ? 'is' : 'is not'} awake, and ${this.isMoving ? 'is' : 'is not'} moving.`;
+//   }
+// }
 
-class Cat extends Animal {
-  constructor(fur, isAwake, isMoving) {
-    super(2, 4, isAwake, isMoving);
-    this.fur = fur;
-  }
-  speak() {
-    super.speak("Meow...");
-  }
-  toString() {
-    return super.toString("Cat");
-  }
-}
+// class Cat extends Animal {
+//   constructor(fur, isAwake, isMoving) {
+//     super(2, 4, isAwake, isMoving);
+//     this.fur = fur;
+//   }
+//   speak() {
+//     super.speak("Meow...");
+//   }
+//   toString() {
+//     return super.toString("Cat");
+//   }
+// }
 
-class Dog extends Animal {
-  constructor(fur, isAwake, isMoving) {
-    super(2, 4, isAwake, isMoving);
-    this.fur = fur;
-  }
-  speak() {
-    super.speak("Woof!");
-  }
-  toString() {
-    return super.toString("Dog");
-  }
-}
+// class Dog extends Animal {
+//   constructor(fur, isAwake, isMoving) {
+//     super(2, 4, isAwake, isMoving);
+//     this.fur = fur;
+//   }
+//   speak() {
+//     super.speak("Woof!");
+//   }
+//   toString() {
+//     return super.toString("Dog");
+//   }
+// }
 
-class Cow extends Animal {
-  constructor(hair, isAwake, isMoving) {
-    super(2, 4, isAwake, isMoving);
-    this.hair = hair;
-  }
-  speak() {
-    super.speak("Moo.");
-  }
-  toString() {
-    return super.toString("Cow");
-  }
-}
+// class Cow extends Animal {
+//   constructor(hair, isAwake, isMoving) {
+//     super(2, 4, isAwake, isMoving);
+//     this.hair = hair;
+//   }
+//   speak() {
+//     super.speak("Moo.");
+//   }
+//   toString() {
+//     return super.toString("Cow");
+//   }
+// }
 
-const cat1 = new Cat("Orange", true, false);
-const cat2 = new Cat("Black and White", false, false);
-const dog1 = new Dog("Gold", true, true);
-const cow1 = new Cow("Brown", true, false);
+// const cat1 = new Cat("Orange", true, false);
+// const cat2 = new Cat("Black and White", false, false);
+// const dog1 = new Dog("Gold", true, true);
+// const cow1 = new Cow("Brown", true, false);
 
-console.log(cat1 instanceof Cat); // true
-console.log(cat1 instanceof Animal); // true
-console.log(cat1 instanceof Object); // true
-console.log(cat1 instanceof Dog); // false
+// console.log(cat1 instanceof Cat); // true
+// console.log(cat1 instanceof Animal); // true
+// console.log(cat1 instanceof Object); // true
+// console.log(cat1 instanceof Dog); // false
+
+
+
+
+
+
+
+class Animal {}
+class Cat extends Animal {}
+class Tabby extends Cat {}
+class SpottedTabby extends Tabby {}
+
+const cat1 = new SpottedTabby();
+let object = cat1;
+
+do {
+  object = Object.getPrototypeOf(object);
+  console.log(object);
+} while (object);
+
+
+
+
+
+
+
+
