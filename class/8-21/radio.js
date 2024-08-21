f370c4a2f6f5f4227302b7e4b050a7257155309b
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 09:47:58 -0400

    give each station a name and songs array

--- class/8-21/radio.js
+++ class/8-21/radio.js
@@ -1,7 +1,19 @@
 const radio = {};
 
 radio.stations = [
-  {},
-  {},
+  {
+    name: 'BBC Radio 1',
+    songs: [
+      'Dance Monkey',
+      'Baby',
+      'Blinding Lights',
+    ],
+  },
+  {
+    name: 'BBC Radio 2',
+    songs: [
+      'Rockstar',
+      'Watermelon Sugar',
+    ],
+  },
 ]
-
