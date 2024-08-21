f94c8e1ae30b00cc8c55dd99e62e47259e930307
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 10:08:49 -0400

    convert to classes

--- class/8-21/radio.js
+++ class/8-21/radio.js
@@ -1,79 +1,44 @@
 class Radio {
-  constructor(stations){
+  constructor(stations = []){
     this.stations = stations;
   }
 
-  addStation(station){
-    this.stations.push(station);
+  changeStation(){
+    this.station = this.stations[Math.floor(Math.random() * this.stations.length)]
+    const song = this.station.songs[Math.floor(Math.random() * this.station.songs.length)]
+    console.log(`Now playing ${song.title} by ${song.artist} on ${this.station.name}`)
+  }
+}
+
+class Station {
+  constructor(name, songs = []){
+    this.name = name;
+    this.songs = songs;
+  }
+}
+
+class Song {
+  constructor(title, artist){
+    this.title = title;
+    this.artist = artist;
   }
 }
 
 const radio = new Radio([
-  {
-    name: 'BBC Radio 1',
-    songs: [
-      {
-	title: 'Rockstar',
-	artist: 'DaBaby',
-      },
-    ],
-  },
-  {
-    name: 'BBC Radio 2',
-    songs: [
-      {
-	title: 'Rockstar',
-	artist: 'DaBaby',
-      },
-    ],
-  },
-  {
-    name: 'BBC Radio 3',
-    songs: [
-      {
-	title: 'Hallelujah',
-	artist: 'Leonard Cohen',
-      },
-    ],
-    
-  },
+  new Station('Pop', [
+    new Song('Shape of You', 'Ed Sheeran'),
+    new Song('Despacito', 'Luis Fonsi')
+  ]),
+  new Station('Rock', [
+    new Song('Bohemian Rhapsody', 'Queen'),
+    new Song('Stairway to Heaven', 'Led Zeppelin')
+  ]),
+  new Station('Jazz', [
+    new Song('Take Five', 'Dave Brubeck'),
+    new Song('So What', 'Miles Davis')
+  ])
 ]);
 
-console.log(radio);
-
-// radio.stations = [
-//   {
-//     name: 'BBC Radio 1',
-//     songs: [
-//       {
-// 	title: 'Rockstar',
-// 	artist: 'DaBaby',
-//       },
-//     ],
-//   },
-//   {
-//     name: 'BBC Radio 2',
-//     songs: [
-//       {
-// 	title: 'Rockstar',
-// 	artist: 'DaBaby',
-//       },
-//     ],
-//   },
-//   {
-//     name: 'BBC Radio 3',
-//     songs: [
-//       {
-// 	title: 'Hallelujah',
-// 	artist: 'Leonard Cohen',
-//       },
-//     ],
-
-//   },
-// ];
-
-// radio.changeStation = function(){
-//   this.station = this.stations[Math.floor(Math.random() * this.stations.length)]
-//   const song = this.station.songs[Math.floor(Math.random() * this.station.songs.length)]
-//   console.log(`Now playing ${song.title} by ${song.artist} on ${this.station.name}`)
-// }
+for (let i = 0; i < 5; i++){
+  radio.changeStation();
+}
