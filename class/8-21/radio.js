c01d7b448c4109d0905caffb020d8ba69db0b2b2
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 10:04:19 -0400

    make radio a class

--- class/8-21/radio.js
+++ class/8-21/radio.js
@@ -1,6 +1,14 @@
-const radio = {};
+class Radio {
+  constructor(stations){
+    this.stations = stations;
+  }
 
-radio.stations = [
+  addStation(station){
+    this.stations.push(station);
+  }
+}
+
+const radio = new Radio([
   {
     name: 'BBC Radio 1',
     songs: [
@@ -29,10 +37,43 @@ radio.stations = [
     ],
     
   },
-];
+]);
 
-radio.changeStation = function(){
-  this.station = this.stations[Math.floor(Math.random() * this.stations.length)]
-  const song = this.station.songs[Math.floor(Math.random() * this.station.songs.length)]
-  console.log(`Now playing ${song.title} by ${song.artist} on ${this.station.name}`)
-}
+console.log(radio);
+
+// radio.stations = [
+//   {
+//     name: 'BBC Radio 1',
+//     songs: [
+//       {
+// 	title: 'Rockstar',
+// 	artist: 'DaBaby',
+//       },
+//     ],
+//   },
+//   {
+//     name: 'BBC Radio 2',
+//     songs: [
+//       {
+// 	title: 'Rockstar',
+// 	artist: 'DaBaby',
+//       },
+//     ],
+//   },
+//   {
+//     name: 'BBC Radio 3',
+//     songs: [
+//       {
+// 	title: 'Hallelujah',
+// 	artist: 'Leonard Cohen',
+//       },
+//     ],
+
+//   },
+// ];
+
+// radio.changeStation = function(){
+//   this.station = this.stations[Math.floor(Math.random() * this.stations.length)]
+//   const song = this.station.songs[Math.floor(Math.random() * this.station.songs.length)]
+//   console.log(`Now playing ${song.title} by ${song.artist} on ${this.station.name}`)
+// }
