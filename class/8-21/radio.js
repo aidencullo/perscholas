ae9b5f58c17bd8c3ccdd1a45b2b069832ffe579a
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 09:57:45 -0400

    implement changeStation method in Radio class

--- class/8-21/radio.js
+++ class/8-21/radio.js
@@ -19,6 +19,20 @@ radio.stations = [
       },
     ],
   },
+  {
+    name: 'BBC Radio 3',
+    songs: [
+      {
+	title: 'Hallelujah',
+	artist: 'Leonard Cohen',
+      },
+    ],
+    
+  },
 ];
 
-
+radio.changeStation = function(){
+  this.station = this.stations[Math.floor(Math.random() * this.stations.length)]
+  const song = this.station.songs[Math.floor(Math.random() * this.station.songs.length)]
+  console.log(`Now playing ${song.title} by ${song.artist} on ${this.station.name}`)
+}
