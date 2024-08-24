b9aed22ed29255fcbcf1c3b09cb3481232a09adf
Aiden Cullo <aidencullo@gmail.com>
2024-08-24 12:29:14 -0400

    unresolved promises

--- class/8-24/microtask.js
+++ class/8-24/microtask.js
@@ -1,12 +1,12 @@
-Promise.resolve().then(console.log);
-// all these will print before the above line
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
+// Promise.resolve().then(console.log);
+// // all these will print before the above line
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
 
 
 
@@ -14,13 +14,31 @@ console.log(1);
 
 
 
-// test your knowledge
-new Promise(resolve => {
-  console.log(1);
-  resolve(2);
-}).then(console.log);
+// // test your knowledge
+// new Promise(resolve => {
+//   console.log(1);
+//   resolve(2);
+// }).then(console.log);
 
-console.log(3);
+// console.log(3);
 
 
-// 1, 3, 2
+// // 1, 3, 2
+
+
+
+
+
+
+
+
+
+
+
+const promise = new Promise(resolve => {
+  return 1;
+});
+
+promise
+  .then(console.log)
+  .catch(console.error);
