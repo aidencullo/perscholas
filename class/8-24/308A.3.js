a373bae06fe69ab000a2692ba067ad999508cd63
Aiden Cullo <aidencullo@gmail.com>
2024-08-24 13:36:46 -0400

    linearize promise handlers

--- class/8-24/308A.3.js
+++ class/8-24/308A.3.js
@@ -31,34 +31,61 @@
 
 
 
-function doSomethingCritical() {
-  return Promise.resolve({ critical: 'data' });
-}
+// function doSomethingCritical() {
+//   return Promise.resolve({ critical: 'data' });
+// }
 
-function doSomethingOptional(data) {
-  return Promise.resolve({ optional: 'data' });
-}
+// function doSomethingOptional(data) {
+//   return Promise.resolve({ optional: 'data' });
+// }
 
-function moreCriticalStuff() {
-  return Promise.resolve({ more: 'data' });
-}
+// function moreCriticalStuff() {
+//   return Promise.resolve({ more: 'data' });
+// }
 
-function doSomethingExtraNice(data) {
-  return Promise.resolve({ extra: 'extra nice data' });
-}
-
-
-doSomethingCritical()
-  .then((result) =>
-    doSomethingOptional(result)
-      .then((optionalResult) => doSomethingExtraNice(optionalResult))
-      .catch((e) => {}),
-  ) // Ignore if optional stuff fails; proceed.
-  .then(() => moreCriticalStuff())
-  .catch((e) => console.error(`Critical failure: ${e.message}`))
-  .then(console.log);
+// function doSomethingExtraNice(data) {
+//   return Promise.resolve({ extra: 'extra nice data' });
+// }
 
 
-Promise.resolve('foo')
-  .then(() => Promise.resolve('bar')) // Return a promise
-  .then(console.log); // Outputs: 'bar'
+// doSomethingCritical()
+//   .then((result) =>
+//     doSomethingOptional(result)
+//       .then((optionalResult) => doSomethingExtraNice(optionalResult))
+//       .catch((e) => {}),
+//   ) // Ignore if optional stuff fails; proceed.
+//   .then(() => moreCriticalStuff())
+//   .catch((e) => console.error(`Critical failure: ${e.message}`))
+//   .then(console.log);
+
+
+// Promise.resolve('foo')
+//   .then(() => Promise.resolve('bar')) // Return a promise
+//   .then(console.log); // Outputs: 'bar'
+
+
+
+
+
+
+new Promise((resolve, reject) => {
+  console.log("Initial");
+
+  resolve();
+})
+  .then(() => {
+    throw new Error("Something failed");
+
+    console.log("Do this");
+  })
+  .catch(() => {
+    console.error("Do that");
+  })
+  .then(() => {
+    console.log("Do this, no matter what happened before");
+  });
+
+
+// initial
+// do that
+// do this...
