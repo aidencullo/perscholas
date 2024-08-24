6798a859f52ab90ac320d79076745f000f81327f
Aiden Cullo <aidencullo@gmail.com>
2024-08-24 11:55:50 -0400

    show microtask queue waits until stack is empty

---
+++ class/8-24/microtask.js
@@ -0,0 +1,9 @@
+Promise.resolve().then(console.log);
+// all these will print before the above line
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
