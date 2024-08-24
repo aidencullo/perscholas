b5ce2db6d038b37606e6e12a0b78b70db5b3b840
Aiden Cullo <aidencullo@gmail.com>
2024-08-24 14:09:39 -0400

    microtask following promise preempts setTimeout in promise

--- class/8-24/308A.3.js
+++ class/8-24/308A.3.js
@@ -94,27 +94,90 @@
 
 
 
-function doSomething() {
-  return Promise.resolve('First thing');
-}
+// function doSomething() {
+//   return Promise.resolve('First thing');
+// }
 
-function doSomethingElse(result) {
-  throw new Error('Fail');
-  return Promise.resolve(result + ' Second thing');
-}
+// function doSomethingElse(result) {
+//   throw new Error('Fail');
+//   return Promise.resolve(result + ' Second thing');
+// }
 
-function doThirdThing(result) {
-  return Promise.resolve(result + ' Third thing');
-}
+// function doThirdThing(result) {
+//   return Promise.resolve(result + ' Third thing');
+// }
 
-function failureCallback(error) {
-  throw new Error('Oh no!');
-  console.error('Error:', error);
-}
+// function failureCallback(error) {
+//   throw new Error('Oh no!');
+//   console.error('Error:', error);
+// }
 
-doSomething()
-  .then((result) => doSomethingElse(result))
-  .then((newResult) => doThirdThing(newResult))
-  .then((finalResult) => console.log(`Got the final result: ${finalResult}`))
-  .catch(failureCallback)
-  .catch((e) => console.error(`Critical failure: ${e.message}`));
+// doSomething()
+//   .then((result) => doSomethingElse(result))
+//   .then((newResult) => doThirdThing(newResult))
+//   .then((finalResult) => console.log(`Got the final result: ${finalResult}`))
+//   .catch(failureCallback)
+//   .catch((e) => console.error(`Critical failure: ${e.message}`));
+
+
+
+
+
+
+
+
+
+
+
+// function func1() {
+//   return Promise.resolve('First thing');
+// }
+
+// function func2() {
+//   return Promise.resolve('Second thing');
+// }
+
+// function func3() {
+//   return Promise.resolve('Third thing');
+// }
+
+// // Promise.all([func1(), func2(), func3()]).then(([result1, result2, result3]) => {
+// //   console.log(result1, result2, result3);
+// // });
+
+
+
+// // [func1, func2, func3]
+// //   .reduce((p, f) => p.then(f), Promise.resolve())
+// //   .then((result3) => {
+// //     console.log(result3);
+// //   });
+
+
+// Promise.resolve()
+//   .then(func1)
+//   .then(func2)
+//   .then(func3)
+//   .then(console.log);
+
+
+// const promise1 = Promise.reject();
+// const promise2 = Promise.reject();
+// const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 0, 'foo'));
+
+// const promises = [promise1, promise2, promise3];
+
+// Promise.any(promises).then((value) => console.log(value));
+
+
+
+
+setTimeout(() => console.log('A'), 0);
+setTimeout(() => console.log('B'), 0);
+setTimeout(() => console.log('C'), 0);
+new Promise((resolve, reject) => {
+  setTimeout(() => console.log('D'), 0);
+  resolve();
+})
+  .then(() => console.log('E'))
+  .then(() => console.log('F'));
