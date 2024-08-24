4bc458a8eebefcb099c427d8570ee670a3e55e82
Aiden Cullo <aidencullo@gmail.com>
2024-08-24 12:49:56 -0400

    chaining promises

---
+++ class/8-24/308A.3.js
@@ -0,0 +1,24 @@
+console.log("One");
+
+setTimeout(() => console.log("Two"), 0);
+
+console.log("Three");
+
+
+
+// Create a Promise that resolves after one second.
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Guess this worked!");
+  }, 1000);
+});
+
+// Add some then() methods to handle additional tasks.
+myPromise
+  .then((x) => x + ' Again?')
+  .then((x) => x + ' Third time!')
+  .then((x) => x + ' Promises are cool.')
+  .catch((err) => {
+    console.error(err);
+  })
+  .then((x) => console.log(x));
