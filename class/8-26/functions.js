5211e4737fcd2a7c0e08e497c6f7fd6a02900be8
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 16:26:05 -0400

    no dup params

---
+++ class/8-26/functions.js
@@ -0,0 +1,17 @@
+// neither of these will work
+
+
+// const exampleFunction = (a, b, a) => {
+//   console.log(a, b)
+// }
+
+// exampleFunction("first", "second", "third")
+
+// function exampleFunction(a, b, a) {
+//   console.log(a, b)
+// }
+
+// exampleFunction("first", "second", "third")
+
+
+
