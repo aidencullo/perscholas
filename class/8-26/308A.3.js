b97b558dc0d931806751b83f5c61bfb0abcc634d
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 16:23:38 -0400

    arguments not available in arrow fn

--- class/8-26/308A.3.js
+++ class/8-26/308A.3.js
@@ -1,36 +1,51 @@
-doSomething()
-  .then((result) => doSomethingElse(result))
-  .then((newResult) => doThirdThing(newResult))
-  .then((finalResult) => console.log(`Got the final result: ${finalResult}`))
-  .catch(failureCallback);
+// doSomething()
+//   .then((result) => doSomethingElse(result))
+//   .then((newResult) => doThirdThing(newResult))
+//   .then((finalResult) => console.log(`Got the final result: ${finalResult}`))
+//   .catch(failureCallback);
 
 
-async function doSomething() {
-  throw new Error('doSomething failed');
-  return Promise.resolve('doSomething');
+// async function doSomething() {
+//   throw new Error('doSomething failed');
+//   return Promise.resolve('doSomething');
+// }
+
+// async function doSomethingElse(result) {
+//   return Promise.resolve(result + ' doSomethingElse');
+// }
+
+// async function doThirdThing(result) {
+//   return Promise.resolve(result + ' doThirdThing');
+// }
+
+// async function failureCallback(error) {
+//   console.error('Error: ', error);
+// }
+
+// async function asyncErrorHandler() {
+//   try {
+//     const something = await doSomething();
+//     const somethingElse = await doSomethingElse(something);
+//     const thirdThing = await doThirdThing(somethingElse);
+//   }
+//   catch (e) {
+//     failureCallback(e);
+//   }
+// }
+
+// asyncErrorHandler();
+
+
+
+
+// function logNumbers(num1, num2) {
+//   console.log(arguments)
+// }
+
+// logNumbers(8, 24)
+
+const logNumbers = (num1, num2) => {
+  console.log(arguments)
 }
 
-async function doSomethingElse(result) {
-  return Promise.resolve(result + ' doSomethingElse');
-}
-
-async function doThirdThing(result) {
-  return Promise.resolve(result + ' doThirdThing');
-}
-
-async function failureCallback(error) {
-  console.error('Error: ', error);
-}
-
-async function asyncErrorHandler() {
-  try {
-    const something = await doSomething();
-    const somethingElse = await doSomethingElse(something);
-    const thirdThing = await doThirdThing(somethingElse);
-  }
-  catch (e) {
-    failureCallback(e);
-  }
-}
-
-asyncErrorHandler();
+// logNumbers(8, 24) // Uncaught ReferenceError: arguments is not defined
