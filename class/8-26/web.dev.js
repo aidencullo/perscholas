cd9fd6882dc6b1f9914a016d9a543858956e4394
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 10:19:09 -0400

    check types of various values in JavaScript

--- class/8-26/web.dev.js
+++ class/8-26/web.dev.js
@@ -1,4 +1,22 @@
 import { expect } from 'chai';
 
-expect(2 + 2).to.equal(4);
-expect(2 + 2).to.not.equal(4);
+expect(typeof null).to.equal('object');
+expect(typeof undefined).to.equal('undefined');
+expect(typeof true).to.equal('boolean');
+expect(typeof false).to.equal('boolean');
+expect(typeof 1).to.equal('number');
+expect(typeof 1.1).to.equal('number');
+expect(typeof 'string').to.equal('string');
+expect(typeof {}).to.equal('object');
+expect(typeof []).to.equal('object');
+expect(typeof function() {}).to.equal('function');
+expect(typeof new Date()).to.equal('object');
+expect(typeof /./).to.equal('object');
+expect(typeof new Error()).to.equal('object');
+expect(typeof Symbol()).to.equal('symbol');
+expect(typeof BigInt(1)).to.equal('bigint');
+expect(typeof new Map()).to.equal('object');
+expect(typeof new Set()).to.equal('object');
+expect(typeof new WeakMap()).to.equal('object');
+expect(typeof new WeakSet()).to.equal('object');
+expect(typeof new Promise(() => {})).to.equal('object');
