2db8940b40168df694b1eceaca1563fa8fafa7cb
Aiden Cullo <aidencullo@gmail.com>
2024-08-27 10:10:25 -0400

    sample get w xhr

--- class/8-27/308A.4/src/index.js
+++ class/8-27/308A.4/src/index.js
@@ -1,1 +1,11 @@
 const request = new XMLHttpRequest();
+
+// example of a GET request
+
+// request.open("GET", "http://www.example.com/myFile.json", true);
+// request.send();
+
+// request.setRequestHeader(
+//     "Content-Type",
+//     "my-MIME-type-here"
+// );
