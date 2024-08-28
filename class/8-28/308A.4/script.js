e0518006866fa4ea9469c7bbaf3d87622f77422a
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 14:24:16 -0400

    POST req w axios

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -6,22 +6,16 @@ async function testRequest() {
   let inputVal = document.getElementById("myInput").value;
   let requestBody = { data: inputVal };
 
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
-    method: 'POST',
-    body: JSON.stringify(requestBody),
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    }
-  });
+  const response = await axios.post('https://jsonplaceholder.typicode.com/todos', 
+				    requestBody, { 
+				      headers: { 
+					'Content-Type': 'application/json; charset=UTF-8' 
+				      }
+				    });
 
-  alertResponse(response);
+  logResponse(response);
 }
 
-async function alertResponse(response) {
-  if (response.ok) {
-    const textData = await response.text();
-    alert(textData);
-  } else {
-    alert("The request returned a status other than 200 OK: " + response.status);
-  }
+async function logResponse(response) {
+  console.log(response.data);
 }
