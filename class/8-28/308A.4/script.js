fedbf8091c418351232f6ded5171aaf449c0c6f3
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 14:36:03 -0400

    time requests with interceptors

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -1,20 +1,24 @@
-const requestInterceptor = axios.interceptors.request.use(request => {
-  console.log('Request sent.');
+axios.interceptors.request.use(request => {
+  request.metadata = request.metadata || {};
+  request.metadata.startTime = new Date().getTime();
   return request;
 });
 
 axios.interceptors.response.use(
   (response) => {
-    // Success: status 200 - 299
-    console.log('Successful response!');
+    response.config.metadata.endTime = new Date().getTime();
+    response.config.metadata.durationInMS = response.config.metadata.endTime - response.config.metadata.startTime;
+
+    console.log(`Request took ${response.config.metadata.durationInMS} milliseconds.`)
     return response;
   },
   (error) => {
-    // Failure: anything outside of status 2XX
-    console.log('Unsuccessful response...');
+    error.config.metadata.endTime = new Date().getTime();
+    error.config.metadata.durationInMS = error.config.metadata.endTime - error.config.metadata.startTime;
+
+    console.log(`Request took ${error.config.metadata.durationInMS} milliseconds.`)
     throw error;
-  }
-);
+  });
 
 document
   .getElementById("myBtn")
@@ -30,8 +34,6 @@ async function testRequest() {
 					'Content-Type': 'application/json; charset=UTF-8' 
 				      }
 				    });
-
-  axios.interceptors.request.eject(requestInterceptor);
 
   logResponse(response);
 }
