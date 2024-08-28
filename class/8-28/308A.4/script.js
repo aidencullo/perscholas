424a22a215daa322a681e2ac6617024c9953a243
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 12:27:56 -0400

    post html input to api server

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -1,28 +1,37 @@
 let request;
 
 document
-    .getElementById("myBtn")
-    .addEventListener("click", testRequest);
+  .getElementById("myBtn")
+  .addEventListener("click", testRequest);
 
 function testRequest() {
-    request = new XMLHttpRequest();
+  request = new XMLHttpRequest();
 
-    if (!request) {
-        alert("Failed to create an XMLHttpRequest Object.");
-        return false;
-    }
+  if (!request) {
+    alert("Failed to create an XMLHttpRequest Object.");
+    return false;
+  }
 
-    request.onreadystatechange = alertResponse;
-    request.open("GET", "test.html");
-    request.send();
+  request.onreadystatechange = alertResponse;
+
+  request.open("POST", "https://jsonplaceholder.typicode.com/todos");
+
+  request.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+
+  let inputVal = document.getElementById("myInput").value;
+  let encodedVal = encodeURIComponent(inputVal);
+  request.send(`data=${encodedVal}`);
 }
 
- function alertResponse() {
-    if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-            alert(request.responseText);
-        } else {
-            alert("The request returned a status other than 200 OK: " + request.status);
-        }
+function alertResponse() {
+  if (request.readyState === XMLHttpRequest.DONE) {
+    if (request.status === 200 || request.status === 201) {
+      alert(request.responseText);
+    } else {
+      alert("The request returned a status other than 200 OK: " + request.status);
     }
+  }
 }
