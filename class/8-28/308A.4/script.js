019adfd8f4bcd3d3da70b6e454682afd295ad541
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 11:50:56 -0400

    add event listener xhr

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -2,4 +2,18 @@ const request = new XMLHttpRequest();
 
 const url = "https://jsonplaceholder.typicode.com/posts";
 request.open("GET", url, true);
+request.setRequestHeader(
+  "Content-Type",
+  "application/x-www-form-urlencoded; charset=UTF-8"
+);
 request.send();
+
+function handleResponse() {
+  if (request.readyState === XMLHttpRequest.DONE) {
+    const response = JSON.parse(request.responseText);
+  } else {
+    console.log("Error");
+  }
+}
+
+request.onreadystatechange = handleResponse;
