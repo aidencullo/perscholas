97567a10727513897c4b1e423b87c361db92f9eb
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 14:42:11 -0400

    information to be immediately available to our handler functions

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -7,18 +7,23 @@ axios.interceptors.request.use(request => {
 axios.interceptors.response.use(
   (response) => {
     response.config.metadata.endTime = new Date().getTime();
-    response.config.metadata.durationInMS = response.config.metadata.endTime - response.config.metadata.startTime;
-
-    console.log(`Request took ${response.config.metadata.durationInMS} milliseconds.`)
+    response.durationInMS = response.config.metadata.endTime - response.config.metadata.startTime;
     return response;
   },
   (error) => {
     error.config.metadata.endTime = new Date().getTime();
-    error.config.metadata.durationInMS = error.config.metadata.endTime - error.config.metadata.startTime;
-
-    console.log(`Request took ${error.config.metadata.durationInMS} milliseconds.`)
+    error.durationInMS = error.config.metadata.endTime - error.config.metadata.startTime;
     throw error;
   });
+
+(async () => {
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+  const { data, durationInMS } = await axios(url);
+  console.log(`Request took ${durationInMS} milliseconds.`);
+  console.log(data);
+})();
+
 
 document
   .getElementById("myBtn")
