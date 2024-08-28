eb8b86f75d31bfa5ae88fe692288056e3bcddfe8
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 14:13:24 -0400

    use fetch API to get data from a server

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -1,32 +1,7 @@
-let request;
-
-document
-  .getElementById("myBtn")
-  .addEventListener("click", testRequest);
-
-function testRequest() {
-  request = new XMLHttpRequest();
-
-  if (!request) {
-    alert("Failed to create an XMLHttpRequest Object.");
-    return false;
-  }
-
-  request.onreadystatechange = alertResponse;
-  request.open("GET", "test.xml");    // Change this to point to our new XML document.
-  request.send();
+async function logJSONData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const jsonData = await response.json();
+    console.log(jsonData);
 }
 
-function alertResponse() {
-  if (request.readyState === XMLHttpRequest.DONE) {
-    if (request.status === 200) {
-      const xmlDoc = request.responseXML;
-      const doc_root = xmlDoc.querySelector("root");
-      let data = doc_root.firstChild.data;
-
-      alert(data);
-    } else {
-      alert("The request returned a status other than 200 OK: " + request.status);
-    }
-  }
-}
+logJSONData();
