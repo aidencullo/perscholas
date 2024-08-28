26f3e2c1de718c160f77f626c07b626883e8154e
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 14:15:38 -0400

    fetch new dog image via fetch api

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -1,7 +1,14 @@
-async function logJSONData() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const jsonData = await response.json();
-    console.log(jsonData);
+const dog = document.getElementById('dog');
+dog.addEventListener("click", getNewDog);
+
+async function getNewDog() {
+  dog.style.cursor = 'wait';
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const jsonData = await response.json();
+  const url = jsonData.message;
+
+  dog.src = url;
+  dog.style.cursor = 'pointer';
 }
 
-logJSONData();
+getNewDog();
