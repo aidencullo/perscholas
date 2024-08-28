af1ac82559d028b6bd5ddd0413921fe8698593a7
Aiden Cullo <aidencullo@gmail.com>
2024-08-28 14:20:11 -0400

    post to external server with fetch

--- class/8-28/308A.4/script.js
+++ class/8-28/308A.4/script.js
@@ -1,14 +1,27 @@
-const dog = document.getElementById('dog');
-dog.addEventListener("click", getNewDog);
+document
+  .getElementById("myBtn")
+  .addEventListener("click", testRequest);
 
-async function getNewDog() {
-  dog.style.cursor = 'wait';
-  const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  const jsonData = await response.json();
-  const url = jsonData.message;
+async function testRequest() {
+  let inputVal = document.getElementById("myInput").value;
+  let requestBody = { data: inputVal };
 
-  dog.src = url;
-  dog.style.cursor = 'pointer';
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    body: JSON.stringify(requestBody),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    }
+  });
+
+  alertResponse(response);
 }
 
-getNewDog();
+async function alertResponse(response) {
+  if (response.ok) {
+    const textData = await response.text();
+    alert(textData);
+  } else {
+    alert("The request returned a status other than 200 OK: " + response.status);
+  }
+}
