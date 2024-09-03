b394ae913715edb865ac20fefbbac57bbed1818a
Aiden Cullo <aidencullo@gmail.com>
2024-09-03 16:17:42 -0400

    bigint number is not affected by rounding errors

---
+++ class/9-3/bigint/test.js
@@ -0,0 +1,8 @@
+const myNumber = 9999999999999999;
+const myBigInt = 9999999999999999n;
+
+console.log(typeof myNumber);
+console.log(typeof myBigInt);
+
+console.log(myNumber);
+console.log(myBigInt);
