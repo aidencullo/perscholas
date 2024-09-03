2c860ddff7c0c343659031fcc54c59cec8df1a11
Aiden Cullo <aidencullo@gmail.com>
2024-09-03 12:15:04 -0400

    I'm a teapot

---
+++ class/9-3/router/router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendStatus(418);
+});
