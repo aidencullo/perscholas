e587f5f4a177d4f7d99400d83660788820193c85
Aiden Cullo <aidencullo@gmail.com>
2024-09-04 09:52:05 -0400

    extract route parameter name

--- class/9-4/morning/myapp/routes/greeting.js
+++ class/9-4/morning/myapp/routes/greeting.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/:name', function(req, res, next) {
+  res.send(`Howdy ${req.params.name}`);
+});
+
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('greetings from the user route');
+  res.send(`Howdy friend`);
 });
 
 module.exports = router;
