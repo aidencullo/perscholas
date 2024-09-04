8a8292b63796dcf2127d20706fb9f0719006469a
Aiden Cullo <aidencullo@gmail.com>
2024-09-04 10:40:43 -0400

    safeguard tip route params

--- class/9-4/morning/myapp/routes/tip.js
+++ class/9-4/morning/myapp/routes/tip.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/:total/:tipPercentage', function(req, res, next) {
-  var total = req.params.total;
-  var tipPercentage = req.params.tipPercentage;
-  var tip = total * tipPercentage / 100;
+  if (isNaN(req.params.total) || isNaN(req.params.tipPercentage)) {
+    res.send('Please provide valid numbers');
+    return;
+  }
+  const total = req.params.total;
+  const tipPercentage = req.params.tipPercentage;
+  const tip = total * tipPercentage / 100;
   res.send('The tip is ' + tip);
 });
 
