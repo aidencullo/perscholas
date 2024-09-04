bb31c3d257cacb3c54d4bbb510e142b22a9c339e
Aiden Cullo <aidencullo@gmail.com>
2024-09-04 11:04:19 -0400

    make beer route more robust

--- class/9-4/morning/myapp/routes/beer.js
+++ class/9-4/morning/myapp/routes/beer.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('beer', { bottles: 99 });
+  res.redirect('/99');
 });
 
 router.get('/:beer', function(req, res, next) {
   if (req.params.beer === '0') {
     res.render('empty');
-    return;
   }
-  res.render('beer', { bottles: req.params.beer });
+  if (!isNaN(req.params.beer) && req.params.beer < 100) {
+    res.render('beer', { bottles: req.params.beer });
+  }
+  next(new Error('Invalid beer count'));
 });
 
 module.exports = router;
