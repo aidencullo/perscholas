3f85921e04a871c1c647a1e8526368b2f5b0c058
Aiden Cullo <aidencullo@gmail.com>
2024-09-10 09:45:44 -1000

    listings route

--- class/9-10/activity/index.js
+++ class/9-10/activity/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import listingsRouter from './routes/listings.js';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +11,12 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.use('/listings', listingsRouter);
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
 });
 
 app.listen(PORT, () => {
