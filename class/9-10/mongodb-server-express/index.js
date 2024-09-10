6cb8ab505999795de11ebebb14cf54f7ee5fc80e
Aiden Cullo <aidencullo@gmail.com>
2024-09-10 05:00:03 -1000

    retrieve single post

--- class/9-10/mongodb-server-express/index.js
+++ class/9-10/mongodb-server-express/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { ObjectId } from 'mongodb';
 
 import db from './db.js';
 
@@ -10,6 +11,15 @@ app.get('/', async (req, res) => {
   let collection  = await db.collection('posts');
   let results = await collection.find({}).limit(1).toArray();
   res.send(results).status(200);
+});
+
+app.get('/:id', async (req, res) => {
+  let collection  = await db.collection('posts');
+  let query = { _id: new ObjectId(req.params.id) };
+  let results = await collection.findOne(query);
+
+  if (!results) res.send('Post not found').status(404);
+  else res.send(results).status(200);
 });
 
 app.listen(port, () => {
