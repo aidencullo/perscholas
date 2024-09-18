76be34215750cd61c66e5fbbb0ac92768266327c
Aiden Cullo <aidencullo@gmail.com>
2024-09-18 16:51:37 -0400

    alternate way to increment reactive variable

--- lessons/320H.3-React_Hooks:useState/vite-project/src/App.tsx
+++ lessons/320H.3-React_Hooks:useState/vite-project/src/App.tsx
@@ -5,16 +5,14 @@ export default function Counter(props) {
   const [counter, setCounter] = useState(0)
 
   // Function to add one to the state
-  const addOne = () => {
-    // sets counter to its current value + 1
-    setCounter(counter + 1)
-  }
-
-  // The h1 displays the counter, and the button runs the addOne() function
-  return (
-      <div>
-	  <h1>{counter}</h1>
-	  <button onClick={addOne}>Click Me to Add One</button>
-      </div>
-  )
+ const addOne = () => {
+     setCounter((counter) => counter + 1)
+ }
+    // The h1 displays the counter, and the button runs the addOne() function
+    return (
+	<div>
+	    <h1>{counter}</h1>
+	    <button onClick={addOne}>Click Me to Add One</button>
+	</div>
+    )
 }
