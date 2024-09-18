c8473ce33a28fb3683517b6a98fde8df4b760a09
Aiden Cullo <aidencullo@gmail.com>
2024-09-18 11:00:11 -0400

    prop drilling

--- lessons/320H.2/vite-react/src/Component.jsx
+++ lessons/320H.2/vite-react/src/Component.jsx
@@ -1,31 +1,7 @@
-import React, { useState } from 'react'
+const Parent = props => <Child cheese="gouda" />
 
-// Component receives function as prop to update parent's state
-const SenderChild = props => {
-  return <button onClick={() => props.update("Goodbye")}>Click Me</button>
-}
+const Child = props => <GrandChild data={props.cheese} />
 
-// Component receives parent's state
-const ReceiverChild = props => {
-  return <h1>{props.value}</h1>
-}
-
-// The parent who passes props to both children
-const Parent = props => {
-  // The State
-  // We'll talk about this "useState" thing (called a "hook") in the next lesson!
-  const [state, setState] = useState("Hello")
-
-  // Function to update state to send to child
-  const updateState = data => setState(data)
-
-  // We pass the function and the state as props to the children
-  return (
-    <div>
-      <ReceiverChild value={state} />
-      <SenderChild update={updateState} />
-    </div>
-  )
-}
+const GrandChild = props => <h1>{props.data}</h1>
 
 export default Parent
