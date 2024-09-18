f9c3f68d2ddee62386dc6b5ff8475771bdc68d92
Aiden Cullo <aidencullo@gmail.com>
2024-09-18 11:11:38 -0400

    spreading props

--- lessons/320H.2/vite-react/src/Component.jsx
+++ lessons/320H.2/vite-react/src/Component.jsx
@@ -1,10 +1,44 @@
-const Parent = props => <Child name="John" age={30} />
+const Parent = props =>
+      <>
+	<Child
+	  person = "john"
+	  size = "large"
+	  isSepia = {true}
+	  thickBorder = {false}
+	/>
+	<Child2
+	  person = "john"
+	  size = "large"
+	  isSepia = {true}
+	  thickBorder = {false}
+        />
+      </>
 
-const Child = ({ name, age = 0 }) => (
-  <div>
-    <h1>{name}</h1>
-    <h2>{age}</h2>
-  </div>
-)
+function Child({ person, size, isSepia, thickBorder }) {
+  return (
+    <div className="card">
+      <Avatar
+        person={person}
+        size={size}
+        isSepia={isSepia}
+        thickBorder={thickBorder}
+      />
+    </div>
+  );
+}
+
+function Child2(props) {
+  return (
+    <div className="card">
+      <Avatar {...props} />
+    </div>
+  );
+}
+
+const Avatar = ({ person, size, isSepia, thickBorder }) => {
+  return (
+    <p> {person} {size} {isSepia} {thickBorder} </p>
+  );
+}
 
 export default Parent
