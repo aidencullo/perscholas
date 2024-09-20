563e39e567e69d40ac221092653a4a0297f8f530
Aiden Cullo <aidencullo@gmail.com>
2024-09-20 10:14:08 -0400

    med and strong requirements

--- activities/9-20/vite-project/src/PasswordStrengthChecker.tsx
+++ activities/9-20/vite-project/src/PasswordStrengthChecker.tsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
 export default function App() {
-    const [password, setPassword] = useState("");
     const [status, setStatus] = useState("")
 
-    const isWeak = (password) => {
-	return password.length < 6;
-    }
-
     const isMedium = (password) => {
-	return password.length >= 6 && password.length < 10;
+	return password.length >= 6 && containsNumber(password) && containsLetter(password);
     }
 
     const isStrong = (password) => {
-	return password.length >= 10;
+	return password.length >= 8 && containsNumber(password) && containsLetter(password) && containsSpecialCharacter(password);
+    }
+
+    const containsNumber = (password) => {
+	return password.match(/[0-9]/);
+    }
+
+    const containsLetter = (password) => {
+	return password.match(/[a-zA-Z]/);
+    }
+
+    const containsSpecialCharacter = (password) => {
+	return password.match(/[^a-zA-Z0-9]/);
     }
 
     const HandleChange = (e) => {
