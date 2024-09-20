2e15dce7f4af7c623ca1a2588518b5f3777e74e5
Aiden Cullo <aidencullo@gmail.com>
2024-09-20 09:56:21 -0400

    stub component

---
+++ activities/9-20/vite-project/PasswordStrengthChecker.tsx
@@ -0,0 +1,12 @@
+  /Users/aiden/Github/perscholas/activities/9-20/vite-project:
+  -rw-r--r--    1 aiden  staff    1607 Sep 20 09:49 README.md
+  -rw-r--r--    1 aiden  staff     734 Sep 20 09:49 eslint.config.js
+  -rw-r--r--    1 aiden  staff     366 Sep 20 09:49 index.html
+  -rw-r--r--    1 aiden  staff  112236 Sep 20 09:50 package-lock.json
+  -rw-r--r--    1 aiden  staff     683 Sep 20 09:49 package.json
+  drwxr-xr-x    3 aiden  staff      96 Sep 20 09:49 public
+  drwxr-xr-x    8 aiden  staff     256 Sep 20 09:54 src
+  -rw-r--r--    1 aiden  staff     552 Sep 20 09:49 tsconfig.app.json
+  -rw-r--r--    1 aiden  staff     119 Sep 20 09:49 tsconfig.json
+  -rw-r--r--    1 aiden  staff     479 Sep 20 09:49 tsconfig.node.json
+  -rw-r--r--    1 aiden  staff     163 Sep 20 09:49 vite.config.ts
---
+++ activities/9-20/vite-project/src/PasswordStrengthChecker.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+	<div>
+	    <h1>Password Strength Checker </h1>
+	</div>
+  );
+}
