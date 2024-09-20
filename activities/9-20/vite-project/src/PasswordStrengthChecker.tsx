18ea16f3602b20ec9f613c1a762debf8bff52706
Aiden Cullo <aidencullo@gmail.com>
2024-09-20 10:07:10 -0400

    check if password is too short

--- activities/9-20/vite-project/src/PasswordStrengthChecker.tsx
+++ activities/9-20/vite-project/src/PasswordStrengthChecker.tsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 
 export default function App() {
     const [password, setPassword] = useState("");
+    const [status, setStatus] = useState("")
 
     const HandleChange = (e) => {
-	setPassword("no")
+	if (e.target.value.length < 6) {
+	    setStatus("Password must be at least 6 characters")
+	} else {
+	    setStatus("Password is strong")
+	}
     }
     
     return (
 	<div>
 	    <h1>Enter your password</h1>
 	    <input onChange={HandleChange} type="password" />
-	    <p>{password}</p>
+	    <p>{status}</p>
 	</div>
     );
 }
