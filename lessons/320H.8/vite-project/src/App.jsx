e5bb77e873189eeaa12fdc773dfe844f31c5571d
Aiden Cullo <aidencullo@gmail.com>
2024-09-24 10:48:02 -0400

    Using BrowserRouter

--- lessons/320H.8/vite-project/src/App.jsx
+++ lessons/320H.8/vite-project/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import App from "./App";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Import BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+// Wrap the App component with the BrowserRouter component to enable the router features.
+ReactDOM.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+  document.getElementById("root")
+);
